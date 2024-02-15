e284184ebf10bfad1493d8b6e20a8255c0446383
WildCatFish <heqianyao@gmail.com>
2024-02-15 18:04:31 +1100

    add api for download file

--- internal/app/handlers/file.go
+++ internal/app/handlers/file.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +11,7 @@ import (
 func UploadHandler(c *gin.Context) {
 	file, err := c.FormFile("file")
 	if err != nil {
-		c.String(http.StatusBadRequest, "Bad request")
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
@@ -24,4 +25,17 @@ func UploadHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{"message": "File uploaded successfully!", "filename": file.Filename})
+}
+
+func DownloadHandler(c *gin.Context) {
+
+	name := c.Param("name")
+	fmt.Println("Received file: ", name)
+
+	if _, err := os.Stat("./tmp/" + name); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File not found!"})
+		return
+	}
+
+	c.FileAttachment("./tmp/"+name, name)
 }
